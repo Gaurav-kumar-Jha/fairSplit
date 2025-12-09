fca93c3c28d3d0f93421d5c6acd1642dac17623f
Gaurav-Kumar-Jha <gaurav09222001@gmail.com>
2025-12-09 22:35:42 +0530

    made css look better

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const expenseRoutes = require('./routes/expenses');
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
 
 // View engine setup
 app.engine('ejs', require('ejs-mate'));
